c5968180174cf719bd5966230297d5d91200bfb4
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2021-12-14 09:47:54 +0100

    eerste commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Ticket v-for="event in events" :key="event.id" :singleEvent="event"></Ticket>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Ticket from "@/components/Ticket.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Ticket,
   },
+  data(){
+    return{
+      events:[{
+        id:1,
+        category:'concert',
+        title: 'Tomorrow Land',
+        description: 'Festival op de Schorre',
+        location: 'Boom',
+        date: 'August 08 2021',
+        time: '09:00'
+      },
+        {
+          id:2,
+          category:'concert',
+          title: 'Rock Werchter',
+          description: 'Festival Rock',
+          location: 'Werchter',
+          date: 'July 04 2021',
+          time: '09:00'
+        }
+      ],
+
+    }
+  }
 };
 </script>
